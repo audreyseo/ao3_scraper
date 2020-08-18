b05b76ffa4527186d9b2d07a0565635cfd91b0f4
audreyseo <audreyleeseo@gmail.com>
2020-08-18 14:16:22 -0400

    Close #28, add search url validator
    Could still need some work (still doesn't validate whether or not you're
    actually trying to go to ao3), and it definitely could use some extra
    validation of language and such (I need to scrape that though for it to
    happen, and that's a whole different project that I will probably
    complete once I've really really refactored scrape.py.)
    
    I got to whip out some functional programming though which is fun.

--- ao3_info.py
+++ ao3_info.py
@@ -1,5 +1,8 @@
 import os
 import re
+from urllib.parse import parse_qs
+#from fandom_scrape import fandom_names_json_name
+import json
 
 search_keys = [
   "query",
@@ -25,6 +28,103 @@ search_keys = [
   "sort_column",
   "sort_direction"
 ]
+
+def validate_ao3_search_url(url):
+  def validate_fandom(entry):
+    if len(entry) == 0:
+      return True
+    names = []
+    with open("fandom_names.json", "r") as f:
+      names_json = json.loads(f.read())
+      if "names" in names_json:
+        names = names_json["names"]
+        pass
+      pass
+    if entry not in names:
+      print("Uh oh, fandom name {} not in list of all fandom names!".format(entry))
+      pass
+    else:
+      print("Good, fandom name {} in list of all fandom names!".format(entry))
+    return entry in names
+  
+  def validate_work_stats(entry):
+    if len(entry) == 0:
+      return True
+    stats_validator = re.compile(r"\s*[><]?\s*\d+\s*(?:-\s*\d+\s*)?")
+    if stats_validator.match(entry):
+      return re.match(r"^\s*[><]\s*\d+\s*$", entry) or re.match(r"^\s*\d+\s*-\s*\d+\s*$", entry) or re.match(r"^\s*\d+\s*$", entry)
+    return False
+
+  def make_forall_predicate(pred):
+    return lambda entries: all(pred(entry) for entry in entries)
+  
+  valid_vals = {
+    "complete": lambda x: x in ["", "T", "F"],
+    "crossover": lambda x: x in ["", "T", "F"],
+    "single_chapter": lambda x: x == ["0"] or x == ["0", "1"],
+    "fandom_names": validate_fandom,
+    "rating_ids": lambda x: x in ["", "9", "10", "11", "12", "13"],
+    "archive_warning_ids": lambda x: x in ["", "14", "17", "18", "16", "19", "20"],
+    "category_ids": lambda x: x in ["", "116", "22", "21", "23", "2246", "24"],
+    "word_count": validate_work_stats,
+    "hits": validate_work_stats,
+    "kudos_count": validate_work_stats,
+    "comments_count": validate_work_stats,
+    "bookmarks_count": validate_work_stats,
+    "sort_column": lambda x: x in ["_score", "authors_to_sort_on", "title_to_sort_on", "created_at", "revised_at", "word_count", "hits", "kudos_count", "comments_count", "bookmarks_count"],
+    "sort_direction": lambda x: x in ["", "asc", "desc"]
+  }
+  valid_vals = {k: (v if k == "single_chapter" else make_forall_predicate(v)) for k,v in valid_vals.items()}
+
+  query_dict = parse_qs(url, keep_blank_values=True)
+  print("{}".format(query_dict))
+  ws = "work_search"
+  find_key = re.compile(r"work_search\[([^\]]*)\]")
+  only_search_params = {find_key.sub(r"\1", k): v for k,v in query_dict.items() if find_key.match(k)}
+  print("{}".format(only_search_params))
+
+  for param in only_search_params:
+    if param in valid_vals:
+      validator = valid_vals[param]
+      if not validator(only_search_params[param]):
+        print("Uh oh, found non-valid value(s) {} of parameter {}".format(only_search_params[param], param))
+        return False
+      else:
+        print("Yay, valid value(s) {} of parameter {}".format(only_search_params[param], param))
+        pass
+      """
+      for k in only_search_params[param]:
+        # They're all lists, for some reason
+        if not validator(k):
+          # Found first non-valid value
+          print("Uh oh, found non-valid value {} of parameter {}".format(k, param))
+          return False
+        else:
+          print("cool, {} is a valid value of {}".format(k, param))
+          pass
+        pass"""
+      pass
+    pass
+  testing_true = [">100", "    >     3430920", "<100", "    <233420   ", "100-1000", "    123   -   12223", "1244950", " 1111 "]
+  testing_false = [" 1000 0000", "<>10000", "<>", "<", ">", "10000>", "10000<", "-1000", "<-10000", ">-34930", "393993-"]
+  for t in testing_true:
+    if validate_work_stats(t):
+      print("Yay! Expected \"{}\" to be validated, was valid.".format(t))
+      pass
+    else:
+      print("Uh oh, expected \"{}\" to be validated, but was not".format(t))
+      pass
+    pass
+  for f in testing_false:
+    if not validate_work_stats(f):
+      print("Yay! Expected \"{}\" to not be validated, was not valid".format(f))
+      pass
+    else:
+      print("Uh oh, expected \"{}\" to not be validated, but was valid".format(f))
+      pass
+    pass
+  return True
+
 
 rating_to_id = {
   "G": 10,
@@ -188,4 +288,8 @@ def ao3_work_search_url(query="",
 
 
 if __name__ == "__main__":
-  print(ao3_work_search_url(page=2))
+  #print(ao3_work_search_url(page=2))
+  validate_ao3_search_url(ao3_work_search_url(page=2))
+  validate_ao3_search_url("https://archiveofourown.org/works/search?utf8=✓&work_search%5Bquery%5D=&work_search%5Btitle%5D=&work_search%5Bcreators%5D=&work_search%5Brevised_at%5D=&work_search%5Bcomplete%5D=&work_search%5Bcrossover%5D=&work_search%5Bsingle_chapter%5D=0&work_search%5Bword_count%5D=2067&work_search%5Blanguage_id%5D=&work_search%5Bfandom_names%5D=Destiny+%28Video+Games%29&work_search%5Brating_ids%5D=&work_search%5Bcategory_ids%5D%5B%5D=116&work_search%5Bcharacter_names%5D=&work_search%5Brelationship_names%5D=&work_search%5Bfreeform_names%5D=&work_search%5Bhits%5D=&work_search%5Bkudos_count%5D=&work_search%5Bcomments_count%5D=&work_search%5Bbookmarks_count%5D=&work_search%5Bsort_column%5D=_score&work_search%5Bsort_direction%5D=desc&commit=Search")
+  validate_ao3_search_url("https://archiveofourown.org/works/search?utf8=✓&work_search%5Bquery%5D=&work_search%5Btitle%5D=&work_search%5Bcreators%5D=&work_search%5Brevised_at%5D=&work_search%5Bcomplete%5D=T&work_search%5Bcrossover%5D=F&work_search%5Bsingle_chapter%5D=0&work_search%5Bsingle_chapter%5D=1&work_search%5Bword_count%5D=&work_search%5Blanguage_id%5D=&work_search%5Bfandom_names%5D=Naruto&work_search%5Brating_ids%5D=&work_search%5Bcategory_ids%5D%5B%5D=116&work_search%5Bcharacter_names%5D=&work_search%5Brelationship_names%5D=&work_search%5Bfreeform_names%5D=&work_search%5Bhits%5D=&work_search%5Bkudos_count%5D=&work_search%5Bcomments_count%5D=&work_search%5Bbookmarks_count%5D=&work_search%5Bsort_column%5D=_score&work_search%5Bsort_direction%5D=desc&commit=Search")
+  
